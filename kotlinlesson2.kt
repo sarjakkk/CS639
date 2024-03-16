139da20edce8f8010f07c0c11bdd241428249d11
Sarjak Kothiya <sarjakkothiyausa@gmail.com>
2024-03-15 20:12:41 -0400

    Add lotlinlesson2.kt

---
+++ kotlinlesson2.kt
@@ -0,0 +1,45 @@
+// This is code of excercise for kotlin lesson 2 
+
+import java.util.*    
+
+//val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+fun main() {
+    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+    // eager, creates a new list
+    val eager = decorations.filter { it[0] == 'p' }
+    println("eager: $eager")
+    // lazy, will wait until asked to evaluate
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+
+    // force evaluation of the lazy list
+    val newList = filtered.toList()
+    println("new list: $newList")
+
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
+    }
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
+
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
+
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+
+    val mysports = listOf("basketball", "fishing", "running")
+    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+    val mylist = listOf(mysports, myplayers, mycities)     // list of lists
+    println("-----")
+    println("Flat: ${mylist.flatten()}")
+}
