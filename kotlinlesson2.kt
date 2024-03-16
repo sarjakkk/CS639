02c729a6655d3df26c967eed1fd7a314ac878c4c
Sarjak Kothiya <sarjakkothiyausa@gmail.com>
2024-03-15 21:25:47 -0400

    Add lotlinlesson2.kt

--- kotlinlesson2.kt
+++ kotlinlesson2.kt
@@ -1,29 +1,20 @@
 // This is code of excercise for kotlin lesson 2 
 
-import java.util.*    
+import java.util.*   
 
-//val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 
 fun main() {
-    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
-
-    // eager, creates a new list
+    println( decorations.filter { it[0] == 'p'})
+    
     val eager = decorations.filter { it[0] == 'p' }
     println("eager: $eager")
-    // lazy, will wait until asked to evaluate
+
     val filtered = decorations.asSequence().filter { it[0] == 'p' }
     println("filtered: $filtered")
 
-    // force evaluation of the lazy list
     val newList = filtered.toList()
     println("new list: $newList")
-
-    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
-        println("access: $it")
-        it
-    }
-    println("-----")
-    println("filtered: ${lazyMap2.toList()}")
 
     val lazyMap = decorations.asSequence().map {
         println("access: $it")
@@ -36,10 +27,33 @@ fun main() {
     println("-----")
     println("all: ${lazyMap.toList()}")
 
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
+    }
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
+
     val mysports = listOf("basketball", "fishing", "running")
     val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
     val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
     val mylist = listOf(mysports, myplayers, mycities)     // list of lists
     println("-----")
     println("Flat: ${mylist.flatten()}")
+}
+
+val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
+
+fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+   return operation(dirty)
+}
+
+fun increaseDirty(start: Int): Int {
+    return start + 1
+}
+
+fun main(args: Array<String>) {
+    val dirtyLevel = 19
+    val updatedDirtyLevel = updateDirty(dirtyLevel) { dirtyLevel -> dirtyLevel + 23}
+    println(updatedDirtyLevel)
 }
